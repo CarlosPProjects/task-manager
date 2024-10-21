1b979202bb12a3a74f765187134752950024cd37
CarlosPProjects <cg9822@gmail.com>
2024-10-21 23:02:20 +0200

    zz

--- src/app/actions/addUser.ts
+++ src/app/actions/addUser.ts
@@ -1,11 +1,9 @@
 "use server";
 
 // import { IUser } from "@/types/global";
-import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
 
 export const addUser = async (formData: FormData) => {
   const { email, name, password } = Object.fromEntries(formData);
@@ -13,13 +11,7 @@ export const addUser = async (formData: FormData) => {
   const hashedPassword = await bcrypt.hash(password as string, 10);
 
   try {
-    await prisma.users.create({
-      data: {
-        email: email as string,
-        name: name as string,
-        password: hashedPassword,
-      },
-    });
+    
     revalidatePath("/");
   } catch (error) {
     console.log(error);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-import { addUser } from "./actions/addUser";
-import { IUser } from "@/types/global";
-
-const prisma = new PrismaClient();
-
 const Home = async () => {
-  const users: IUser[] = await prisma.users.findMany();
-
   return (
     <div>
-      <h1>Todos</h1>
-      <form action={addUser} className="flex flex-col gap-4">
-        <input type="text" name="name" placeholder="Add new user" />
-        <input type="email" name="email" placeholder="Add new email" />
-        <input type="password" name="password" placeholder="Add new password" />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
+      Hello World
     </div>
-  );
+  )
 };
 
 export default Home;
