cb17b961d04e22c83ce7ab2f1863993c294369ca
CarlosPProjects <cg9822@gmail.com>
2024-10-23 22:05:17 +0200

    "Added categories fetching and display, and a form with a select field for categories"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { getUserTasks } from "./actions/task";
+import { getCategories } from "./actions/categories";
 
 export default async function Home() {
   const tasks = await getUserTasks();
+  const categories = await getCategories();
+
+  
 
   return (
     <div>
@@ -19,6 +23,24 @@ export default async function Home() {
           <div key={task.id}>{task.name}</div>
         ))}
       </div>
+      <h2>Categories</h2>
+      <div>
+        {categories?.map((category) => (
+          <div key={category.id}>{category.title}</div>
+        ))}
+      </div>
+      
+      <form action="" className="flex flex-col gap-4 max-w-48">
+        <input type="text" />
+        <select name="" id="">
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.title}
+            </option>
+          ))}
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
