3a14ed5679802209541a31e1280ac4fe622f2743
CarlosPProjects <cg9822@gmail.com>
2024-10-24 09:08:24 +0200

    "Added Prisma schema models and updated task action to include user authentication check"

--- src/app/actions/task.ts
+++ src/app/actions/task.ts
@@ -2,11 +2,16 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { PrismaClient } from "@prisma/client";
+import Error from "next/error";
 
 const prisma = new PrismaClient();
 
 export const createTask = async (name: string) => {
   const user = await currentUser();
+
+  if(!user){
+    // return new Error();
+  }
 
   await prisma.tasks.create({
     data: {
