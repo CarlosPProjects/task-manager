67ef92ff857f4ef2d220c15aec8052759ea66d20
CarlosPProjects <cg9822@gmail.com>
2024-10-24 09:11:43 +0200

    "Updated createTask function to handle authentication and error cases"

--- src/app/actions/task.ts
+++ src/app/actions/task.ts
@@ -2,21 +2,28 @@
 
 import { currentUser } from "@clerk/nextjs/server";
 import { PrismaClient } from "@prisma/client";
-import Error from "next/error";
 
 const prisma = new PrismaClient();
 
 export const createTask = async (name: string) => {
   const user = await currentUser();
 
-  if(!user){
-    // return new Error();
+  if (!user) {
+    throw new Error('Usuario no autenticado');
   }
 
-  await prisma.tasks.create({
-    data: {
-      name,
-      user_id: user?.id,
-    },
-  });
+  try {
+    const task = await prisma.tasks.create({
+      data: {
+        name,
+        user_id: user?.id,
+      },
+    });
+
+    return {success: true, task}
+
+  } catch (error) {
+    console.error('Error al crear la tarea:', error);
+    return { success: false, error: 'Error al crear la tarea' };
+  }
 };
