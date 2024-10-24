8aed8bc18e709b53f0737e3da6c522e02ba87d24
CarlosPProjects <cg9822@gmail.com>
2024-10-24 20:57:00 +0200

    "Remove categories fetching and selection from CreateTaskForm"

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import { getCategories } from "@/app/actions/categories";
 import { createTask } from "@/app/actions/task";
-import React, { useEffect } from "react";
+import React from "react";
 
 const CreateTaskForm = () => {
-  const [categories, setCategories] = React.useState<ICategory[]>([]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
 
     const data = Object.fromEntries(formData);
-
-    if (!data.category || !data.task) return;
 
     const {task, error} = await createTask(data.task.toString());
 
@@ -26,25 +22,9 @@ const CreateTaskForm = () => {
 
   };
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const categories = await getCategories();
-      setCategories(categories);
-    };
-
-    fetchCategories();
-  }, []);
-
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-48">
       <input type="text" name="task" />
-      <select name="category">
-        {categories.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.title}
-          </option>
-        ))}
-      </select>
       <input type="submit" value="Submit" />
     </form>
   );
