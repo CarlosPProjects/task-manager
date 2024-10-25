4f2d8e6b62666036a0c376a7c91adfbc0ee64cba
CarlosPProjects <cg9822@gmail.com>
2024-10-25 21:51:54 +0200

    mmm

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,14 @@
+import { TTask } from '@/utils/types/task';
+import { type FC } from 'react'
+
+interface Props {
+  task: TTask
+}
+
+const TaskCard: FC<Props> = ({ task }) => {
+  return (
+    
+  )
+}
+
+export default TaskCard;
