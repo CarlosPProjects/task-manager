97db2e1d4d01ba13af0456773eade2892e9318ad
CarlosPProjects <cg9822@gmail.com>
2024-10-26 18:33:16 +0200

    "Update CSS classes for sign-in, sign-up, and user-profile pages"

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-8">
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <SignIn />
     </div>
   );
