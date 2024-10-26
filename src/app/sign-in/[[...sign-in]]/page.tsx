5321340a322143218cb9889fbf5019a91c017be1
CarlosPProjects <cg9822@gmail.com>
2024-10-26 18:41:37 +0200

    "Update sign-in and sign-up page layouts to use min-h-screen and grid"

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="min-h-screen grid place-content-center px-4 sm:px-6 lg:px-8 py-8">
       <SignIn />
     </div>
   );
